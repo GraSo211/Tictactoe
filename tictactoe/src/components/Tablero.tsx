eaafa9c9508ae72151db100cb110995e56bb2a1c
Santiago <santigs211@gmail.com>
2025-04-26 19:39:59 -0300

    resolving conflicts with web responsive

--- tictactoe/src/components/Tablero.tsx
+++ tictactoe/src/components/Tablero.tsx
@@ -11,7 +11,7 @@ export default function Tablero({ tabla, marcar, array }: Props) {
 
     return (
         <>
-            <div className=" bg-[#D4C9BE]   grid  grid-cols-3 grid-rows-3 gap-0.5 size-[250px] xl:size-[500px]  ">
+            <div className=" bg-[#D4C9BE]   grid  grid-cols-3 grid-rows-3 gap-[2px] 2xl:gap-[1px]  size-[250px] 2xl:size-[500px]  ">
                 {
                     tabla.map((_, filaIndex)=>{
                         return <Celda key={filaIndex} index={filaIndex} marcar={marcar} array={array}></Celda>
