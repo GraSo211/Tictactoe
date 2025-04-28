53535901fc5f2a51a940a28ca26df72de18ab582
Santiago <santigs211@gmail.com>
2025-04-28 00:10:21 -0300

    adding tsx for autoreload

--- websocket/dist/index.js
+++ websocket/dist/index.js
@@ -1,21 +1,10 @@
-import express,{ Request, Response } from 'express';
-
-
-
+import express from 'express';
 const port = process.env.PORT || 3000;
-
 const app = express();
-
-
-
-
-app.get("/",(req: Request , res: Response)=>{
-    res.send("Hello World!!!")
-})
-
-
-
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
-
+//# sourceMappingURL=index.js.map
