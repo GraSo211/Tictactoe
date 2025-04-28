761368d2edf4b10b70e29ffc60843c5b8347d7d7
Santiago <santigs211@gmail.com>
2025-04-27 23:49:41 -0300

    initializing project

---
+++ websocket/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { Request, Response } from 'express';
+
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+
+
+
+app.get("/",(req: Request , res: Response)=>{
+    res.send("Hello World!")
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
