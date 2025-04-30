faccc28971b07a025685a2fa2e509728d1fd15f5
Santiago <santigs211@gmail.com>
2025-04-29 23:52:19 -0300

    feat: :sparkles: adding socket io server

--- websocket/dist/index.js
+++ websocket/dist/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 const port = process.env.PORT || 3000;
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const server = createServer(app);
+const io = new Server(server);
+io.on("connection", (socket) => {
+    console.log("Usuario conectado");
 });
-app.listen(port, () => {
+app.get("/", (req, res) => {
+    res.send("Hello World!!!");
+});
+server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 //# sourceMappingURL=index.js.map
--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -1,13 +1,19 @@
 import express,{ Request, Response } from 'express';
 
-
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+const server = createServer(app);
+const io = new Server(server);
 
 
+io.on("connection", (socket) => {
+    console.log("Usuario conectado")
+});
 
 app.get("/",(req: Request , res: Response)=>{
     res.send("Hello World!!!")
@@ -15,7 +21,7 @@ app.get("/",(req: Request , res: Response)=>{
 
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
